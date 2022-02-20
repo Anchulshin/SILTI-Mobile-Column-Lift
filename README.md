6fe4287dbec587a7ec28ec16b64243d1387cfc72
Anchulshin <silti@silti.org>
2022-02-20 15:53:04 +0900

    third commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# SILTI Mobile Column lift Project
+
+
+본 프로그램은 이동이 가능한 컬럼리프트 개발 프로젝트
+ 
+
--- silti_route.py
+++ silti_route.py
@@ -1,6 +1,6 @@
 # SocketIO - 채팅기능
 from flask_socketio import SocketIO
-from flask import Flask, jsonify, render_template, request, url_for, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SECRET KEY']='silti'
@@ -10,15 +10,8 @@ socketio = SocketIO(app)
 def sessions():
     return render_template('index.html')
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
 
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="192.168.0.132", debug=True, port=9999)
+    socketio.run(app, host="192.168.0.6", debug=True, port=9999)
